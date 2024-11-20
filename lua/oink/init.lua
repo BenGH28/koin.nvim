57e14e1f7b4ddb528b3d37c83deeedc94ae9a431
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-11-19 22:05:14 -0700

    add floating window and basic user configuration

--- lua/oink/init.lua
+++ lua/oink/init.lua
@@ -1,18 +1,53 @@
-M = {}
-
 local Oink = {}
 Oink.__index = Oink
 
-function Oink.show(opts)
-  -- local height = vim.api.nvim_win_get_height(0) / 2
-  -- local width = vim.api.nvim_win_get_width(0) / 2
-  -- local default = { relative = 'win', row = width, col = height, width = width, height = height, border = 'single' }
+local default_opts = {
+  float = true,             -- false to split
+  split_direction = "left", -- left | right | above | below
+  float_size = 0.85,
+  split_size = 0.5,
+}
 
-  local config = { split = "left" }
+function Oink:new(opts)
+  local instance = setmetatable({}, Oink)
+  opts = vim.tbl_extend("force", default_opts, opts)
+  instance.opts = opts
+  return instance
+end
+
+function Oink:dbg_opts()
+  print(vim.inspect(self.opts))
+end
+
+function Oink:show(args)
+  local win_height = math.ceil(vim.api.nvim_win_get_height(0) * self.opts.float_size)
+  local win_width = math.ceil(vim.api.nvim_win_get_width(0) * self.opts.float_size)
+
+  local height = vim.api.nvim_win_get_height(0)
+  local width = vim.api.nvim_win_get_width(0)
+  local row = (height - win_height) / 2 - 1
+  local col = (width - win_width) / 2
+
+  local config = {}
+  if self.opts.float then
+    config = {
+      relative = "editor",
+      row = row,
+      col = col,
+      width = win_width,
+      height = win_height,
+      border = "rounded",
+      style = "minimal"
+    }
+  else
+    config = { split = self.opts.split_direction }
+  end
+
   local bufnr = vim.api.nvim_create_buf(true, false)
   local win_id = vim.api.nvim_open_win(bufnr, true, config)
   vim.api.nvim_set_current_win(win_id)
-  vim.fn.termopen(opts.args, {
+  vim.cmd("startinsert")
+  vim.fn.termopen(args.args, {
     on_exit = function()
       vim.api.nvim_win_close(win_id, true)
       vim.api.nvim_buf_delete(bufnr, { force = true })
@@ -20,12 +55,19 @@ function Oink.show(opts)
   })
 end
 
-local function cmds()
-  vim.api.nvim_create_user_command("Oink", Oink.show, { nargs = 1 })
+local function cmds(oink)
+  vim.api.nvim_create_user_command("Oink", function(opts)
+    oink:show(opts)
+  end, { nargs = 1 })
 end
 
-function M.setup()
-  cmds()
+
+M = {}
+
+function M.setup(opts)
+  opts = opts or {}
+  local oink = Oink:new(opts)
+  cmds(oink)
 end
 
 return M
