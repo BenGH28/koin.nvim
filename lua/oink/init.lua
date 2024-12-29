19a39100d3dd896efbd99308ea3924b552b2ad9e
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-12-29 15:58:57 -0700

    remove OOP-ness

--- lua/oink/init.lua
+++ lua/oink/init.lua
@@ -10,27 +10,14 @@ local default_opts = {
   }
 }
 
---- @class Oink
---- @field new fun(self: Oink, opts: table): Oink
---- @field show fun(self: Oink, cmd: string)
---- @field opts table
-local Oink = {}
 
----constructor
----@param opts table
----@return Oink
-function Oink:new(opts)
-  opts = vim.tbl_extend("force", default_opts, opts or {})
-  local obj = { opts = opts }
-  obj = setmetatable(obj, { __index = self })
-  return obj
-end
 
 ---show a window with shell command inside of it
 ---@param cmd string
-function Oink:show(cmd)
-  local win_height = math.ceil(vim.api.nvim_win_get_height(0) * self.opts.window.size)
-  local win_width = math.ceil(vim.api.nvim_win_get_width(0) * self.opts.window.size)
+local show = function(cmd, opts)
+  local options = vim.tbl_extend("force", default_opts, opts or {})
+  local win_height = math.ceil(vim.api.nvim_win_get_height(0) * options.window.size)
+  local win_width = math.ceil(vim.api.nvim_win_get_width(0) * options.window.size)
 
   local height = vim.api.nvim_win_get_height(0)
   local width = vim.api.nvim_win_get_width(0)
@@ -38,24 +25,24 @@ function Oink:show(cmd)
   local col = (width - win_width) / 2
 
   local config = {}
-  if self.opts.float then
+  if options.float then
     config = {
       relative = "editor",
       row = row,
       col = col,
       width = win_width,
       height = win_height,
-      border = self.opts.window.border,
+      border = options.window.border,
       style = "minimal",
     }
   else
-    local direction = self.opts.split.direction
+    local direction = options.split.direction
     config = {
       split = direction
     }
   end
 
-  local bufnr = vim.api.nvim_create_buf(true, false)
+  local bufnr = vim.api.nvim_create_buf(true, true)
   local win_id = vim.api.nvim_open_win(bufnr, true, config)
   vim.api.nvim_set_current_win(win_id)
   vim.cmd("startinsert")
@@ -63,18 +50,23 @@ function Oink:show(cmd)
     on_exit = function()
       vim.api.nvim_win_close(win_id, true)
       vim.api.nvim_buf_delete(bufnr, { force = true })
-    end
+    end,
+    on_stdout = function(id, data, name)
+      if string.match("command not found", data[1]) then
+        vim.notify("unknown command: " .. cmd)
+      end
+    end,
   })
 end
 
-M = {}
-
+M = {
+  show = show,
+}
 function M.setup(opts)
-  opts = opts or {}
-  local oink = Oink:new(opts)
+  opts = vim.tbl_extend("force", default_opts, opts or {})
   vim.api.nvim_create_user_command("Oink", function(cmd_opts)
     local cmd = cmd_opts.args
-    oink:show(cmd)
+    show(cmd, opts)
   end, { nargs = 1 })
 end
 
